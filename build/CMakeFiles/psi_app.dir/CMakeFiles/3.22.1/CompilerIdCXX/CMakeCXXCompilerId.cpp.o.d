59011b851a006c5da6e92aabf741ff394b750451
RajeshRk18 <kannar432@gmail.com>
2024-07-12 21:48:01 +0530

    remove build dir

--- build/CMakeFiles/psi_app.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/psi_app.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/username/cpp/PSI-Experiment/build/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
